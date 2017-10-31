cb3cebdce23f7a50489ed2f594e0bf632838dd5a
Aron Wouters <lionar@mail.com>
2017-10-31 15:17:24 +0100

    Initial aliases.

---
+++ aliases.php
@@ -0,0 +1,20 @@
+<?php
+
+class_alias ( IsoCodes\Bban::class, 'ISO\BBAN' );
+class_alias ( IsoCodes\Cif::class, 'ISO\CIF' );
+class_alias ( IsoCodes\CreditCard::class, 'ISO\creditcard' );
+class_alias ( IsoCodes\Ean13::class, 'ISO\EAN13' );
+class_alias ( IsoCodes\IP::class, 'ISO\IP' );
+class_alias ( IsoCodes\Iban::class, 'ISO\IBAN' );
+class_alias ( IsoCodes\Insee::class, 'ISO\INSEE' );
+class_alias ( IsoCodes\Isbn10::class, 'ISO\ISBN10' );
+class_alias ( IsoCodes\Nif::class, 'ISO\NIF' );
+class_alias ( IsoCodes\OrganismeType12NormeB2::class, 'ISO\organismeType12NormeB2' );
+class_alias ( IsoCodes\Siren::class, 'ISO\SIREN' );
+class_alias ( IsoCodes\Siret::class, 'ISO\SIRET' );
+class_alias ( IsoCodes\Ssn::class, 'ISO\SSN' );
+class_alias ( IsoCodes\StructuredCommunication::class, 'ISO\structuredCommunication' );
+class_alias ( IsoCodes\SwiftBic::class, 'ISO\SWIFTBIC' );
+class_alias ( IsoCodes\Uknin::class, 'ISO\UKNIN' );
+class_alias ( IsoCodes\Vat::class, 'ISO\VAT' );
+class_alias ( IsoCodes\ZipCode::class, 'ISO\zipcode' );
